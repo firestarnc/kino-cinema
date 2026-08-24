bab62f79a7c0fa64d91341ec546e9a8ec038def1
firestarnc <115705609+firestarnc@users.noreply.github.com>
2026-08-24 06:57:05 +0100

    updating package

--- src/lib/private-booking.ts
+++ src/lib/private-booking.ts
@@ -41,7 +41,7 @@ export const BLOCKBUSTER_PACKAGES: PrivatePackage[] = [
     id: "couple",
     name: "Couple Package",
     capacity: "1-2 persons",
-    priceNaira: 150000,
+    priceNaira: 150,
     perks: [
       "A bottle of wine",
       "Unlimited popcorn",
@@ -86,7 +86,7 @@ export const MOVIE_PACKAGES: PrivatePackage[] = [
     id: "couple",
     name: "Couple Package",
     capacity: "1-2 persons",
-    priceNaira: 50000,
+    priceNaira: 150,
     perks: [
       "One bottle of wine",
       "Unlimited velvet popcorn",
